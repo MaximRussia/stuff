eb27991bebb984dcb382c0a482482ad79d20786e
MaximRussia <opengl88@gmail.com>
2017-07-16 16:11:33 +0400

    merge

--- ctci_short/Chapter 16  Threads and Locks/merge.cpp
+++ ctci_short/Chapter 16  Threads and Locks/merge.cpp
@@ -1,110 +1,49 @@
+
 #include <iostream>
-#include <thread>
 #include <vector>
-#include <mutex>
-#include <future>
 #include <algorithm>
+#include <thread>
+#include <future>
+#include <ctime>
 using namespace std;
 
-template<typename Iter>
-void mergesort_mt1(Iter begin, Iter end,
-	unsigned int N = std::thread::hardware_concurrency()) {
-	auto len = std::distance(begin, end);
-	if (len < 2)
-		return;
-
-	Iter mid = std::next(begin, len / 2);
-	if (N > 1) {
-		auto fn = std::async(mergesort_mt1<Iter>, begin, mid, N - 2);
-		mergesort_mt1(mid, end, N - 2);
-		fn.wait();
-	} else {
-		mergesort_mt1(begin, mid, 0);
-		mergesort_mt1(mid, end, 0);
+template<class T>
+void parallel_sort(T * data, int len, int threads = 1) {
+	if (len < threads) {
+		std::sort(data, data + len, std::less<T>());
+        return;
 	}
 
-	std::inplace_merge(begin, mid, end);
-}
-
-template<typename Iter>
-void mergesort_mt2(Iter begin, Iter end,
-	unsigned int N = std::thread::hardware_concurrency()) {
-	auto len = std::distance(begin, end);
-	if (len <= 1024) {
-		std::sort(begin, end);
-		return;
-	}
-
-	Iter mid = std::next(begin, len / 2);
-	if (N > 1) {
-		auto fn = std::async(mergesort_mt2<Iter>, begin, mid, N - 2);
-		mergesort_mt2(mid, end, N - 2);
-		fn.wait();
-	} else {
-		mergesort_mt2(begin, mid, 0);
-		mergesort_mt2(mid, end, 0);
-	}
-
-	std::inplace_merge(begin, mid, end);
-}
-
-template<typename Iter>
-void mergesort_mt3(Iter begin, Iter end,
-	unsigned int N = std::thread::hardware_concurrency()) {
-	auto len = std::distance(begin, end);
-	if (len <= 1024 || N < 2) {
-		std::sort(begin, end);
-		return;
-	}
-
-	Iter mid = std::next(begin, len / 2);
-	auto fn = std::async(mergesort_mt3<Iter>, begin, mid, N - 2);
-	mergesort_mt3(mid, end, N - 2);
-	fn.wait();
-	std::inplace_merge(begin, mid, end);
+	auto future = std::async(parallel_sort<T>, data, len / 2, threads);
+	parallel_sort(data + len / 2, len / 2, threads);
+	future.wait();
+	std::inplace_merge(data, data + len / 2, data + len, std::less<T>());
 }
 
 int main() {
-	using namespace std::chrono;
 
-	std::random_device rd;
-	std::mt19937 rng(rd());
-	std::uniform_int_distribution<unsigned int> dist(0, std::numeric_limits<unsigned int>::max());
+	const int NUM = 999999;
 
-	std::vector<unsigned int> v, back(9999999);
+	srand(time(0));
+	vector<int> arr;
+	for (int i = 0; i < NUM; i++) {
+		arr.push_back(rand() % NUM);
+	}
 
-	std::cout << "Generating...\n";
-	std::generate_n(back.begin(), back.size(), [&]() {return dist(rng); });
+	{
+		time_t t1 = clock();
+		parallel_sort(&arr[0], arr.size(), std::thread::hardware_concurrency());
+		time_t t2 = clock();
+		cout << float(t2 - t1) / CLOCKS_PER_SEC << endl;
+	}
 
-	time_point<system_clock> t0, t1;
+	{
+		time_t t1 = clock();
+		parallel_sort(&arr[0], arr.size());
+		time_t t2 = clock();
+		cout << float(t2 - t1) / CLOCKS_PER_SEC << endl;
+	}
 
-	v = back;
-	std::cout << "std::sort: ";
-	t0 = system_clock::now();
-	std::sort(v.begin(), v.end());
-	t1 = system_clock::now();
-	std::cout << duration_cast<milliseconds>(t1 - t0).count() << "ms\n";
-
-	v = back;
-	std::cout << "mergesort_mt1: ";
-	t0 = system_clock::now();
-	mergesort_mt1(v.begin(), v.end());
-	t1 = system_clock::now();
-	std::cout << duration_cast<milliseconds>(t1 - t0).count() << "ms\n";
-
-	v = back;
-	std::cout << "mergesort_mt2: ";
-	t0 = system_clock::now();
-	mergesort_mt2(v.begin(), v.end());
-	t1 = system_clock::now();
-	std::cout << duration_cast<milliseconds>(t1 - t0).count() << "ms\n";
-
-	v = back;
-	std::cout << "mergesort_mt3: ";
-	t0 = system_clock::now();
-	mergesort_mt3(v.begin(), v.end());
-	t1 = system_clock::now();
-	std::cout << duration_cast<milliseconds>(t1 - t0).count() << "ms\n";
 
 	return 0;
 }
