cbb7ccb4d2b38d69136cd9f9ac56a4134bb1a94a
MaximRussia <opengl88@gmail.com>
2017-07-21 01:27:29 +0400

    refactor

--- adv_data.cpp
+++ adv_data.cpp
@@ -110,95 +110,6 @@ void MinHeap::print() {
 
 }
 
-////////////////////////
-// TRIE
-////////////////////////
-class Node {
-public:
-	Node() { mContent = ' '; mMarker = false; }
-	Node(char ch) { mContent = ch; mMarker = false; }
-	~Node() { mChildren.clear(); }
-	char content() { return mContent; }
-	bool wordMarker() { return mMarker; }
-	void setWordMarker() { mMarker = true; }
-	Node* findChild(char c);
-	void appendChild(Node* child) { mChildren[child->content()] = child; }
-private:
-	char mContent;
-	bool mMarker;
-	map<char, Node*> mChildren;
-};
-
-class Trie {
-public:
-	Trie();
-	~Trie();
-	void addWord(string s);
-	bool searchWord(string s);
-private:
-	Node* root;
-};
-
-Node* Node::findChild(char c) {
-	if(mChildren.find(c) != mChildren.end()) {
-        return mChildren[c];
-	}
-
-	return NULL;
-}
-
-Trie::Trie() {
-	root = new Node();
-}
-
-Trie::~Trie() {
-	delete root; root = NULL;
-}
-
-void Trie::addWord(string s) {
-	Node* current = root;
-
-	if (s.length() == 0) {
-		current->setWordMarker(); // an empty word
-		return;
-	}
-
-	for (int i = 0; i < s.length(); i++) {
-		Node* child = current->findChild(s[i]);
-		if (child != NULL) {
-			current = child;
-		} else {
-			Node* tmp = new Node(s[i]);
-			current->appendChild(tmp);
-			current = tmp;
-		}
-		if (i == s.length() - 1)
-			current->setWordMarker();
-	}
-}
-
-
-bool Trie::searchWord(string s) {
-	Node* current = root;
-
-	while (current != NULL) {
-		for (int i = 0; i < s.length(); i++) {
-			Node* tmp = current->findChild(s[i]);
-			if (tmp == NULL)
-				return false;
-			current = tmp;
-		}
-
-		if (current->wordMarker())
-			return true;
-		else
-			return false;
-	}
-
-	return false;
-}
-////////////////////////
-
 int main() {
 
 	cout << "/////////////////////////////////" << endl;
@@ -218,36 +129,6 @@ int main() {
 	h.extractMin();
 	cout << h.getMin() << endl;
 	h.print();
-
-	cout << endl;
-
-	cout << "/////////////////////////////////" << endl;
-	cout << "// TRIE" << endl;
-	cout << "/////////////////////////////////" << endl;
-
-	/**
-			 *
-			 H       B
-			 E       A
-			 L       L
-			 L       L^
-			 O^      O
-			         O
-			         N^
-    **/
-
-	Trie* trie = new Trie();
-	trie->addWord("Hello");
-	trie->addWord("Balloon");
-	trie->addWord("Ball");
-
-    cout << "Found Hell " << trie->searchWord("Hell") << endl;
-    cout << "Found Hello " << trie->searchWord("Hello") << endl;
-    cout << "Found Helloo " << trie->searchWord("Helloo") << endl;
-    cout << "Found Ball " << trie->searchWord("Ball") << endl;
-    cout << "Found Balloon " << trie->searchWord("Balloon") << endl;
-
-	delete trie;
 
 	cout << endl;
 
--- test_bed.cpp
+++ test_bed.cpp
@@ -13,6 +13,48 @@
 using namespace std;
 
 /////////////////////////////////
+// BITS
+/////////////////////////////////
+
+void swap_bits(int &a, int &b) {
+	a ^= b ^= a ^= b;
+}
+
+int getBin(int n, int i) {
+	return n & (1 << i);
+}
+
+int setBin(int n, int i) {
+	return n | (1 << i);
+}
+
+int clearBit(int n, int i) {
+	int mask = ~(1 << i);
+	return n & mask;
+}
+
+string toString(int num) {
+	string res;
+	while (num != 0) {
+		res.push_back('0' + (num & 1));
+		num >>= 1;
+	}
+	std::reverse(res.begin(), res.end());
+	return res;
+}
+
+
+int toInt(string str) {
+	if (str.size() > sizeof(int) * 8) return 0;
+	int res = 0;
+	int size = str.size() - 1;
+	for (int i = size; i >= 0; --i) {
+		res |= ((str[i] - '0') << (size - i));
+	}
+	return res;
+}
+
+/////////////////////////////////
 // MEMORY
 /////////////////////////////////
 
