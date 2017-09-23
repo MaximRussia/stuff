0d1b9549c7a4f24b9910b385a134cdb4064ab7b1
MaximRussia <opengl88@gmail.com>
2017-09-23 22:39:11 +0300

    ref

--- trie.cpp
+++ trie.cpp
@@ -1,113 +1,97 @@
+#include <atomic>
+#include <future>
+#include <thread>
 #include <iostream>
+#include <vector>
+#include <queue>
 #include <map>
 using namespace std;
 
-class Node {
-public:
-    Node(char ch) { mContent = ch; mMarker = false; }
-    ~Node() {}
-    char content() { return mContent; }
-    void setContent(char c) { mContent = c; }
-    bool wordMarker() { return mMarker; }
-    void setWordMarker() { mMarker = true; }
-    Node* findChild(char c);
-    void appendChild(Node* child) { mChildren[child->content()] = child; }
+struct Node {
+    Node(char ch) : ch(ch) {
+    }
 
-private:
-    char mContent;
-    bool mMarker;
-    map<char, Node*> mChildren;
+    Node* is(char ch) {
+         map<char, Node*>::const_iterator it = children.find(ch);
+         if(it != children.end()) {
+            return it->second;
+         }
+         return NULL;
+    }
+
+    Node* add(char ch) {
+        children[ch] = new Node(ch);
+        return children[ch];
+    }
+
+    char ch;
+    bool end;
+    map<char, Node*> children;
 };
 
 class Trie {
-public:
-    Trie();
-    ~Trie();
-    void addWord(string s);
-    bool searchWord(string s);
-    void deleteWord(string s);
-private:
-    Node* root;
+    Node *root;
+
+    public:
+    Trie() : root(NULL){
+        root = new Node('\0');
+    }
+
+    bool find(string s) {
+        Node* current = root;
+        for(int i = 0; i < s.size(); i++) {
+            Node* find = current->is(s[i]);
+            if(find) {
+                current = find;
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    void add(string s) {
+        Node* current = root;
+        for(int i = 0; i < s.size(); i++) {
+            Node* find = current->is(s[i]);
+            if(!find) {
+                current = current->add(s[i]);
+                continue;
+            }
+            current = find;
+        }
+    }
+
+    void BFS() {
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()) {
+            Node* r = q.front(); q.pop();
+            for(auto &i : r->children) {
+                cout << i.first << " ";
+                q.push(i.second);
+            }
+            cout << endl;
+        }
+    }
 };
 
-Node* Node::findChild(char c) {
+int main(void) {
 
-    map<char, Node*>::const_iterator it = mChildren.find(c);
-    if(it != mChildren.end())
-        return it->second;
+    Trie t;
 
-    return NULL;
-}
+    t.add("dog");
+    t.add("daaag");
+    t.add("friend");
 
-Trie::Trie() {
-    root = new Node(' ');
-}
+    t.BFS();
 
-Trie::~Trie() {
-    // Free memory
-}
+    cout << t.find("dog") << endl;
+    cout << t.find("dag") << endl;
+    cout << t.find("daag") << endl;
+    cout << t.find("daaag") << endl;
+    cout << t.find("daaa") << endl;
+    cout << t.find("frnd") << endl;
 
-void Trie::addWord(string s) {
-    Node* current = root;
-
-    if (s.empty()) {
-        current->setWordMarker(); // an empty word
-        return;
-    }
-
-    for (int i = 0; i < s.length(); i++) {
-        Node* child = current->findChild(s[i]);
-        if (child) {
-            current = child;
-        } else {
-            Node* tmp = new Node(s[i]);
-            current->appendChild(tmp);
-            current = tmp;
-        }
-        if (i == s.length() - 1)
-            current->setWordMarker();
-    }
-}
-
-
-bool Trie::searchWord(string s) {
-    Node* current = root;
-
-    while (current) {
-        for (int i = 0; i < s.length(); i++) {
-            Node* tmp = current->findChild(s[i]);
-            if (!tmp) return false;
-            current = tmp;
-        }
-
-        return current->wordMarker();
-    }
-
-    return false;
-}
-
-
-// Test program
-int main() {
-    Trie* trie = new Trie();
-    trie->addWord("Hello");
-    trie->addWord("Balloon");
-    trie->addWord("Ball");
-
-    if (trie->searchWord("Hell"))
-        cout << "Found Hell" << endl;
-
-    if (trie->searchWord("Hello"))
-        cout << "Found Hello" << endl;
-
-    if (trie->searchWord("Helloo"))
-        cout << "Found Helloo" << endl;
-
-    if (trie->searchWord("Ball"))
-        cout << "Found Ball" << endl;
-
-    if (trie->searchWord("Balloon"))
-        cout << "Found Balloon" << endl;
-
-    delete trie;
+    return 0;
 }
