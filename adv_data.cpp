fbc878fa133129c35bf73d9ed53ece95d68b32b9
MaximRussia <opengl88@gmail.com>
2017-10-16 23:52:30 +0300

    ref

--- adv_data.cpp
+++ adv_data.cpp
@@ -110,6 +110,83 @@ void MinHeap::print() {
 
 }
 
+//////////////////////////////////
+// Fenwick Tree. A Fenwick Tree is a data structure that represents
+// an array of n numbers. It supports adjusting the i-th element in O(log n)
+// time, and computing the sum of numbers in the range i..j in O(log n)
+// time. It only needs O(n) space.
+//////////////////////////////////
+
+struct fenwick_tree {
+    int n;
+    vector<int> data;
+
+    fenwick_tree(int _n) : n(_n), data(vector<int>(n)) { }
+
+    void update(int at, int by) {
+        while (at < n) data[at] += by, at |= at + 1;
+    }
+
+    int query(int at) {
+        int res = 0;
+        while (at >= 0) res += data[at], at = (at & (at + 1)) - 1;
+        return res;
+    }
+
+    int rsq(int a, int b) { return query(b) - query(a - 1); }
+};
+
+struct fenwick_tree_sq {
+    int n;
+    fenwick_tree x1, x0;
+
+    fenwick_tree_sq(int _n) :
+        n(_n), x1(fenwick_tree(n)), x0(fenwick_tree(n)) { }
+
+    void update(int x, int m, int c) {
+        x1.update(x, m);
+        x0.update(x, c);
+    }
+
+    int query(int x) { return x*x1.query(x) + x0.query(x); }
+};
+
+void range_update(fenwick_tree_sq &s, int a, int b, int k) {
+    s.update(a, k, k * (1 - a));
+    s.update(b+1, -k, k * b);
+}
+
+int range_query(fenwick_tree_sq &s, int a, int b) {
+    return s.query(b) - s.query(a - 1);
+}
+
+//////////////////////////////
+//  Union-Find. An implementation of the Union-Find disjoint sets
+//  data structure.
+//////////////////////////////
+struct union_find {
+    vector<int> p;
+
+    union_find(int n) : p(n, -1) { }
+
+    int find(int x) {
+        return p[x] < 0 ? x : p[x] = find(p[x]);
+    }
+
+    bool unite(int x, int y) {
+        int xp = find(x);
+        int yp = find(y);
+        if (xp == yp) return false;
+        if (p[xp] > p[yp]) swap(xp, yp);
+        p[xp] += p[yp];
+        p[yp] = xp;
+        return true;
+    }
+
+    int size(int x) { return -p[find(x)]; }
+};
+
+
 int main() {
 
 	cout << "/////////////////////////////////" << endl;
