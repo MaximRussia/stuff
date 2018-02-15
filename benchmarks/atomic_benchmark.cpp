9b258a47ea1e1a4238c51fcbb94ffa65cbf6b22f
MaximRussia <opengl88@gmail.com>
2018-02-16 01:10:35 +0300

    ref

--- benchmarks/atomic_benchmark.cpp
+++ benchmarks/atomic_benchmark.cpp
@@ -9,7 +9,6 @@
 #include <ctime>
 using namespace std;
 
-
 #define OPERATIONS 999
 #define REPEAT 5
 
@@ -99,15 +98,67 @@ void bench_atomic(int Threads) {
 	std::cout << "atomic with " << Threads << " threads, ms = " << float(t2 - t1) / CLOCKS_PER_SEC << std::endl;
 }
 
+void bench_spin(int Threads) {
+
+	class SpinLock {
+		std::atomic_flag locked = ATOMIC_FLAG_INIT;
+	public:
+		void lock() {
+			while (locked.test_and_set(std::memory_order_acquire)) { ; }
+		}
+		void unlock() {
+			locked.clear(std::memory_order_release);
+		}
+	};
+
+	class SpinLockRAII {
+		SpinLock& lock;
+
+	public:
+		SpinLockRAII(SpinLock& _lock) : lock(_lock) {
+			lock.lock();
+		}
+
+		~SpinLockRAII() {
+			lock.unlock();
+		}
+	};
+
+	SpinLock mutex;
+
+	time_t t1 = clock();
+
+	for (int i = 0; i < REPEAT; ++i) {
+		float counter = 0.0f;
+
+		std::vector<std::thread> threads;
+
+		for (int i = 0; i < Threads; ++i) {
+			threads.push_back(std::thread([&]() {
+				for (int i = 0; i < OPERATIONS; ++i) {
+					SpinLockRAII guard(mutex);
+					counter += pow(i, pow(i, 60));
+				}
+			}));
+		}
+
+		for (auto& thread : threads) {
+			thread.join();
+		}
+	}
+
+	time_t t2 = clock();
+
+	std::cout << "spin_lock with " << Threads << " threads, ms = " << float(t2 - t1) / CLOCKS_PER_SEC << std::endl;
+
+}
+
 #define bench(name)\
 	name(1); \
-	name(2); \
 	name(4); \
-	name(8); \
 	name(16); \
-	name(32); \
 	name(64); \
-	name(128);
+	name(256);
 
 int main() {
 
@@ -118,6 +169,7 @@ int main() {
 	bench(bench_atomic);
 	bench(bench_lock);
 	bench(bench_lock_guard);
+	bench(bench_spin);
 
 	return 0;
 }
